bc0815af842ab3f5681c2fe81bb5ca907da67b5a
Dmityi <palladin-dim@mail.ru>
2022-09-03 20:57:58 +0700

    Добавлена задача 8

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-    if (number % 2 == 1)
+    if (number % 2 == -1)
     {
     Console.WriteLine("Число " + number + " нет");
     }
